aa8c86ee6d5e6d1b43ec6635ea3a350fafbee025
dalychoi <dalychoi@gmail.com>
2018-12-29 11:44:37 +0900

    Initial commit

---
+++ src/main/java/hello/Quote.java
@@ -0,0 +1,34 @@
+package hello;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Quote {
+	
+	private String type;
+	private Value value;
+	
+	public Quote() {
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public Value getvalue() {
+		return value;
+	}
+
+	public void setvalue(Value value) {
+		this.value = value;
+	}
+
+	@Override
+	public String toString() {
+		return "Quote {type='" + type + "'" + ", value=" + value + "}";
+	}
+}
